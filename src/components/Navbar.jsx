9aaa13dbeb7b42941f053e02895f7eb68b436017
youssefelyou <youssefelyourizi36@gmail.com>
2023-04-24 19:33:35 +0100

    icon and name updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,14 +100,6 @@ const Navbar = () => {
                             Github <FaGithub size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                        <a
-                            className='flex justify-between items-center w-full text-gray-300'
-                            href='/'
-                        >
-                            Resume <BsFillPersonLinesFill size={30} />
-                        </a>
-                    </li>
                 </ul>
             </div>
         </div>
