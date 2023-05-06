6efebfba43be2fe37539a19c739fd3d7b99c15c4
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-06 21:40:08 +0100

    Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'> Submit the form below or shoot me an email</p>
             </div>
+
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
                 <textarea className='bg-[#ccd6f6] p-2' name="message" rows="3" placeholder='Message'></textarea>
