1b14a7bf387bd853a6b1bb7c9c57af158b87138a
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-06 21:42:10 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
                     <p className='text-4xl font-bold inline border-b-4 border-blue-600 '>Skills</p>
-                    <p className='py-4'> These are the technologies I have worked with</p>
+                    <p className='py-4'> These are the technologies I have worked with </p>
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8'>
