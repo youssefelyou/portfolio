c558deedca639e79cfd75d7ab7a0d49a9f780ec0
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-06 21:38:41 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,11 @@ import {
     FaTimes,
     FaGithub,
     FaLinkedin,
-
 } from 'react-icons/fa';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav);
-
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
